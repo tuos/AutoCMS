f18981c3fe4fb5ce2c48e388babc4b4713c746b1
Eric <eric.appelt@gmail.com>
2014-10-26 14:05:10 -0500

    new way of handling submission timestamps to avoid race conditions, config variable for days to keep logs, logharvester removes old timestamps

--- JobRecord.py
+++ JobRecord.py
@@ -1,3 +1,5 @@
+import re
+
 class JobRecord:
   'information about a specific AutoCMS job'
 
@@ -10,7 +12,9 @@ class JobRecord:
     self.endTime = None
     self.exitCode = None
     self.errorString = None
-    
+    self.inputFile = None
+    self.logFile = None    
+
   def runTime(self):
     return self.endTime - self.startTime
 
@@ -24,9 +28,28 @@ class JobRecord:
       return False
 
   def isComplete(self):
-    if self.startTime is not None and self.endTime is not None:
+    if self.logFile is not None:
       return True
     else:
       return False
 
- 
+  def parseOutput(self,logFileName):
+    self.logFile = logFileName
+    # assume jobs failed unless success token is found
+    self.exitCode = 1
+    with open(logFileName,'r') as logFile:
+      log = logFile.read().splitlines()
+    for line in log:
+      # note - the SKIM_TEST token needs to be eventually changed to AUTOCMS_TEST
+      if( re.match(r'timestamp_begin=',line)):
+        self.startTime = int(line.replace('timestamp_begin=',''))
+      if( re.match(r'timestamp_end=',line)):
+        self.endTime = int(line.replace('timestamp_end=',''))
+      if( re.match(r'SKIM_TEST: Running on node',line)):
+        self.node = line.replace('SKIM_TEST: Running on node ','')
+      if( re.match(r'SKIM_TEST: Will use input file ',line)):
+        self.inputFile = line.replace('SKIM_TEST: Will use input file ','')
+      if( re.search(r'ALL TESTS SUCCESSFUL',line)):
+        self.exitCode = 0 
+      if( re.match(r'SKIM_TEST:.*ERROR',line)):
+        self.errorString = line.replace('SKIM_TEST: ','')
--- logharvester.py
+++ logharvester.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import cPickle as pickle
 from JobRecord import JobRecord
@@ -9,6 +10,14 @@ config = AutoCMSUtil.LoadConfiguration('autocms.cfg')
 testdir = config['AUTOCMS_BASEDIR']+"/"+config['AUTOCMS_TEST_NAME']
 os.chdir(testdir)
 
+# set current time, and timestamp before which old
+# logs and stamps will be purged
+now = int(time.time())
+if int(config['AUTOCMS_LOG_LIFETIME']) > 0:
+  purgetime = now - 3600 * 24 * int(config['AUTOCMS_LOG_LIFETIME'])
+else:
+  purgetime = now - 3600 * 24 * 7
+
 # initialize JobRecord dictionary, or load saved state
 autocms_pkl = 'records.pickle'
 if os.path.isfile(autocms_pkl):
@@ -17,16 +26,43 @@ else:
   records = dict()
 
 # create new entries for jobs submitted today, but not 
-# found in the dictionary
-now = int(time.time())
+# found in the dictionary, delete old stamps
+#
+# note: to avoid locking and race conditions, the submitter 
+# creates new stamps in files of the form "newstamp.1414308601"
+# containing the jobid and timestamp. These are 
+# addded to the "submission.stamps" file and removed 
+# only by this script, which also removes old stamps from the 
+# file. 
+#
+with open('submission.stamps','a') as stampFile:
+  for newStampFileName in filter(lambda x:re.match(r'newstamp', x), os.listdir('.')):
+    with open(newStampFileName,'r') as newStampFile:
+      newStamp = newStampFile.read().strip()
+    print>>stampFile, newStamp
+    os.remove(newStampFileName)
 with open('submission.stamps','r') as stampFile:
   stampsIn = stampFile.read().splitlines()
-for line in stampsIn:
+for line in stampsIn[:]:
   timestamp = int(line.split()[1])
   jobid = line.split()[0].replace('.vmpsched','')
   if now - timestamp < 60*60*24 :
     if timestamp not in records:
       records[timestamp] = JobRecord(timestamp,jobid)
+  if timestamp < purgetime:
+    stampsIn.remove(line) 
 
+stampFile = open('submission.stamps','w') 
+for line in stampsIn:
+  print>>stampFile, line
+stampFile.close()
 
+# Harvest information from log for jobs not yet 
+# known to be completed
+for job in records:
+  if not records[job].isComplete():
+    jobLogFile = config['AUTOCMS_TEST_NAME']+'.pbs.o'+str(records[job].jobid)
+    if os.path.isfile(jobLogFile):
+      records[job].parseOutput(jobLogFile)
 
+ 
