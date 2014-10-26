88fe6d3e50ac70430ea613c505a8489c9c961b83
Eric <eric.appelt@gmail.com>
2014-10-26 11:08:06 -0500

    python logharvester reads in new submission stamps

--- logharvester.py
+++ logharvester.py
@@ -1,16 +1,16 @@
 import os
 import time
 import cPickle as pickle
-import JobRecord
+from JobRecord import JobRecord
 import AutoCMSUtil
 
 # load configuration and enter test directory
-config = AutoCMSUtil.LoadConfiguration("autocms.cfg")
+config = AutoCMSUtil.LoadConfiguration('autocms.cfg')
 testdir = config['AUTOCMS_BASEDIR']+"/"+config['AUTOCMS_TEST_NAME']
 os.chdir(testdir)
 
 # initialize JobRecord dictionary, or load saved state
-autocms_pkl = "records.pickle"
+autocms_pkl = 'records.pickle'
 if os.path.isfile(autocms_pkl):
   records = pickle.load( open(autocms_pkl, "rb") )
 else:
@@ -20,5 +20,13 @@ else:
 # found in the dictionary
 now = int(time.time())
 with open('submission.stamps','r') as stampFile:
-  
+  stampsIn = stampFile.read().splitlines()
+for line in stampsIn:
+  timestamp = int(line.split()[1])
+  jobid = line.split()[0].replace('.vmpsched','')
+  if now - timestamp < 60*60*24 :
+    if timestamp not in records:
+      records[timestamp] = JobRecord(timestamp,jobid)
+
+
 
