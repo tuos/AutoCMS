4df3d8bb4aadbf90e5ac2da9599be34efff7486a
Eric <eric.appelt@gmail.com>
2015-02-11 10:56:39 -0600

    refactor autocms.sh, now all commands run through autocms.sh to call python, the .bashrc is run so absolute paths to basic commands are removed

--- autocms.sh
+++ autocms.sh
@@ -2,66 +2,122 @@
 
 main ()
 {
-
-
-  if [[ ${commandline_args[1]} = "print" ]]
-  then
-    print_autocms_crontab
-    echo
-    echo "Crontab lines for autocms below:"
-    echo "=================================================="
-    cat autocms.crontab
-    echo "=================================================="
-    echo "These lines are also saved in the file \"autocms.crontab\""
-    echo
-    exit 0
+  source autocms.cfg
+  if [ -f ~/.bashrc ]; then
+	. ~/.bashrc
+  elif [ -f /etc/bashrc ]; then
+        . 
   fi
 
+  case "${commandline_args[1]}" in
+  
+    print) autocms_print
+      ;;
+    start) autocms_start
+      ;;
+    stop) autocms_stop
+      ;;
+    submit) autocms_submit
+      ;;
+    logharvest) autocms_logharvest
+      ;;
+    report) autocms_report
 
-  if [[ ${commandline_args[1]} = "start" ]]
-  then  
-    crontab_overwrite_warning
-    if [[ -f crontab_host.txt ]]
-    then
-      AUTOCMS_HOST=`cat crontab_host.txt`
-      echo "ERROR: autocms instance already running on $AUTOCMS_HOST"
-      echo "Exiting..."
-    exit 1
-    fi
-    echo "Starting autocms..."
-    echo $HOSTNAME > crontab_host.txt
-    print_autocms_crontab
-    crontab autocms.crontab
-    exit 0
-  fi
+  esac
 
-  if [[ ${commandline_args[1]} = "stop" ]]
-  then
-    crontab_overwrite_warning
-    if [[ ! -f crontab_host.txt ]]
-    then
-      echo "No running autocms instance detected. Exiting..."
-      exit 1
-    fi
-    AUTOCMS_HOST=`cat crontab_host.txt`
-    if [[ ! $AUTOCMS_HOST = $HOSTNAME ]]
-    then
-      echo "Attempting to stop autocms on $HOSTNAME "
-      echo "but this instance of autocms is running on $AUTOCMS_HOST. "
-      echo "Log into $HOSTNAME and try again."
-      exit 1
-    fi
-    echo "Stopping autocms..." 
-    crontab -r
-    rm crontab_host.txt
-    exit 0
-  fi
-
-  echo "Usage \"autocms.sh start\" or \"autocms.sh stop\" "
+  echo "Usage \"autocms.sh command [testname]\" "
   echo "Please see: "
   echo "  https://wiki.accre.vanderbilt.edu/foswiki/bin/view/VandyCMS/AutoCMS"
   echo "for additional documentation."
 
+}
+
+autocms_print ()
+{
+
+  print_autocms_crontab
+  echo
+  echo "Crontab lines for autocms below:"
+  echo "=================================================="
+  cat autocms.crontab
+  echo "=================================================="
+  echo "These lines are also saved in the file \"autocms.crontab\""
+  echo
+  exit 0
+
+}
+
+autocms_start ()
+{
+  crontab_overwrite_warning
+  if [[ -f crontab_host.txt ]]
+  then
+    AUTOCMS_HOST=`cat crontab_host.txt`
+    echo "ERROR: autocms instance already running on $AUTOCMS_HOST"
+    echo "Exiting..."
+  exit 1
+  fi
+  echo "Starting autocms..."
+  echo $HOSTNAME > crontab_host.txt
+  print_autocms_crontab
+  crontab autocms.crontab
+  exit 0
+}
+
+autocms_stop ()
+{
+  crontab_overwrite_warning
+  if [[ ! -f crontab_host.txt ]]
+  then
+    echo "No running autocms instance detected. Exiting..."
+    exit 1
+  fi
+  AUTOCMS_HOST=`cat crontab_host.txt`
+  if [[ ! $AUTOCMS_HOST = $HOSTNAME ]]
+  then
+    echo "Attempting to stop autocms on $HOSTNAME "
+    echo "but this instance of autocms is running on $AUTOCMS_HOST. "
+    echo "Log into $HOSTNAME and try again."
+    exit 1
+  fi
+  echo "Stopping autocms..." 
+  crontab -r
+  rm crontab_host.txt
+  exit 0
+
+}
+
+autocms_submit ()
+{
+  if [ -d "${AUTOCMS_BASEDIR}/${commandline_args[2]}" ]; then
+    $AUTOCMS_BASEDIR/submitter.sh ${commandline_args[2]} 
+    exit 0
+  else
+    echo "Request to submit non-existent test ${commandline_args[2]} failed"
+    exit 1
+  fi 
+}
+
+autocms_report ()
+{
+  if [ -d "${AUTOCMS_BASEDIR}/${commandline_args[2]}" ]; then
+    python reporter.py ${commandline_args[2]}
+    exit 0
+  else
+    echo "Request to report non-existent test ${commandline_args[2]} failed"
+    exit 1
+  fi
+}
+
+autocms_logharvest ()
+{
+  if [ -d "${AUTOCMS_BASEDIR}/${commandline_args[2]}" ]; then
+    python logharvester.py ${commandline_args[2]}
+    exit 0
+  else
+    echo "Request to harvest non-existent test ${commandline_args[2]} failed"
+    exit 1
+  fi
 }
 
 print_autocms_crontab ()
@@ -77,14 +133,14 @@ print_autocms_crontab ()
   for TESTNAME in $( echo $AUTOCMS_TEST_NAMES | tr ":" "\n" ); do
     echo "count: $COUNT subwait: ${SUBWAIT[$COUNT]}"
     if [ ${SUBWAIT[$COUNT]} -lt 60 ]; then
-      echo "*/${SUBWAIT[$COUNT]} * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/submitter.sh $TESTNAME"  >> autocms.crontab
-      echo "0,10,20,30,40,50 * * * * cd $AUTOCMS_BASEDIR && /usr/local/bin/python logharvester.py $TESTNAME" >> autocms.crontab
-      echo "5,15,25,35,45,55 * * * * cd $AUTOCMS_BASEDIR && /usr/local/bin/python reporter.py $TESTNAME" >> autocms.crontab
+      echo "*/${SUBWAIT[$COUNT]} * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh submit $TESTNAME"  >> autocms.crontab
+      echo "0,10,20,30,40,50 * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh logharvest $TESTNAME" >> autocms.crontab
+      echo "5,15,25,35,45,55 * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh report $TESTNAME" >> autocms.crontab
     else
       SUBWAIT[$COUNT]=$(( ${SUBWAIT[$COUNT]} / 60 )) 
-      echo "0 */${SUBWAIT[$COUNT]} * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/submitter.sh $TESTNAME"  >> autocms.crontab
-      echo "10,40 * * * * cd $AUTOCMS_BASEDIR && /usr/local/bin/python logharvester.py $TESTNAME" >> autocms.crontab
-      echo "20,50 * * * * cd $AUTOCMS_BASEDIR && /usr/local/bin/python reporter.py $TESTNAME" >> autocms.crontab
+      echo "0 */${SUBWAIT[$COUNT]} * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh submit $TESTNAME"  >> autocms.crontab
+      echo "10,40 * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh logharvest $TESTNAME" >> autocms.crontab
+      echo "20,50 * * * * cd $AUTOCMS_BASEDIR && $AUTOCMS_BASEDIR/autocms.sh report $TESTNAME" >> autocms.crontab
     fi
     (( COUNT++ ))
   done
@@ -108,5 +164,4 @@ crontab_overwrite_warning()
 }
 
 commandline_args=("$@")
-source autocms.cfg
 main
--- submitter.sh
+++ submitter.sh
@@ -5,12 +5,10 @@ source `pwd`/autocms.cfg
 AUTOCMS_TEST_NAME=$1
 
 AUTODIR=$AUTOCMS_BASEDIR
-#export MOABHOMEDIR=/usr/scheduler/config/moab
 
 # Determine number of jobs in the queue
 
-#NUMJOBS=`/usr/scheduler/moab/bin/showq -w group=$AUTOCMS_GNAME | grep -c $AUTOCMS_UNAME`
-NUMJOBS=`/usr/scheduler/slurm/bin/squeue -h --user=$AUTOCMS_UNAME --account=$AUTOCMS_GNAME | wc -l`
+NUMJOBS=`squeue -h --user=$AUTOCMS_UNAME --account=$AUTOCMS_GNAME | wc -l`
 
 if [ $NUMJOBS -ge $AUTOCMS_MAXENQUEUE ]; then
   exit 0
@@ -32,7 +30,7 @@ cd $AUTODIR/$AUTOCMS_TEST_NAME
 export AUTOCMS_COUNTER=$SEQ
 export AUTOCMS_CONFIGFILE=$AUTODIR/autocms.cfg
 
-SUBID_MESSAGE=`/usr/scheduler/slurm/bin/sbatch  --account=$AUTOCMS_GNAME $AUTODIR/$AUTOCMS_TEST_NAME/$AUTOCMS_TEST_NAME.slurm -export=AUTOCMS_COUNTER,AUTOCMS_CONFIGFILE  2>&1`
+SUBID_MESSAGE=`sbatch  --account=$AUTOCMS_GNAME $AUTODIR/$AUTOCMS_TEST_NAME/$AUTOCMS_TEST_NAME.slurm -export=AUTOCMS_COUNTER,AUTOCMS_CONFIGFILE  2>&1`
 SUBMIT_STATUS=$?
 SUBID=`echo $SUBID_MESSAGE | sed -e "s/Submitted batch job //"`
 NOW=`date`
