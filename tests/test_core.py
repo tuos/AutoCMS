bea423263b7289366e6163104f2ee9b7c180d83c
Eric <eric.appelt@gmail.com>
2015-03-29 10:29:13 -0500

    reorganize tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,22 @@
-"""Test the AutoCMS JobRecord class methods."""
+"""Test the AutoCMS core functionality."""
 
 import unittest
 
-from autocms.core import load_configuration
-from autocms.core import JobRecord
+from autocms.core import (JobRecord,
+                          load_configuration)
+
+class TestConfiguration(unittest.TestCase):
+    """Test that the configuration is loaded correctly."""
+
+    def setUp(self):
+        self.config = load_configuration('tests/data/autocms.cfg.unittest')
+
+    def test_cmsrun_timeout(self):
+        self.assertEqual(self.config['SKIMTEST_CMSRUN_TIMEOUT'],'14400')
+
+    def test_input_file_token(self):
+        self.assertEqual(self.config['AUTOCMS_input_file_TOKEN'],
+                         'AutoCMS: input file ')
 
 
 class TestJobRecord(unittest.TestCase):
@@ -28,6 +41,7 @@ class TestJobRecord(unittest.TestCase):
                              self.config)
         record_copy = eval(repr(record)) 
         self.assertEqual(repr(record),repr(record_copy))
+
 
 if __name__ == '__main__':
     unittest.main()
