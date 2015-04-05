63bf473c3b8ef1aac88188634f9df5ab6e4e822e
Eric <eric.appelt@gmail.com>
2015-04-04 22:33:30 -0500

    reimplement record printer with argparse module

--- print_records.py
+++ print_records.py
@@ -1,18 +1,24 @@
 """Print all records in the pickle for the specified test"""
 
-import os
 import sys
+import argparse
 
-from autocms.core import (JobRecord, load_records)
+from autocms.core import (load_configuration, load_records)
 
 
 def main():
-    """Print all records corresponding to test given as an  argument"""
-    os.chdir(sys.argv[1])
-    records = load_records('records.pickle')
-    for key in records:
-        print str(records[key])+"\n"
-
+    """Print all records corresponding to test given as an argument"""
+    parser = argparse.ArgumentParser(description='Submit one or more jobs.')
+    parser.add_argument('testname', help='test directory')
+    parser.add_argument('-c', '--configfile', type=str,
+                        default='autocms.cfg',
+                        help='AutoCMS configuration file name')
+    args = parser.parse_args()
+    config = load_configuration(args.configfile)
+    records = load_records(args.testname,config)
+    for job in records:
+        print str(job)+'\n'
+    return 0
 
 if __name__ == '__main__':
     status = main()
