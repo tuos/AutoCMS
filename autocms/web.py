373cf920845b38820786d67af37d24a26db385c8
Eric <eric.appelt@gmail.com>
2015-04-05 08:38:35 -0500

    bare-bones reimplemented reporting finished, system now runs with new implementation, but graphics and reporting features are to follow.

--- autocms/web.py
+++ autocms/web.py
@@ -1,25 +1,25 @@
 """AutoCMSWebpage class to simplify generating a web page."""
 
-import sys
 import os
 import time
 
+from .core import (load_records, save_records)
+
 
 class AutoCMSWebpage(object):
+    """Class for building and writing a page to report on an AutoCMS test."""
 
-    def __init__(self, testname, path, records, config):
+    def __init__(self, records, testname, config):
         """Construct new AutoCMSWebpage object.
 
         Arguments:
-             testname - name of the autocms test directory
-             path - name of output file to write html
              records - dictionary of JobRecords
+             testname - name of the autocms test directory
              config - autocms configuration dictionary"""
-        self.testname = testname
         self.records = records
+        self.testname = testname
         self.config = config
-        self.output_path = path
-        self.page = "" 
+        self.page = ""
 
     def begin_page(self):
         """Write head and open webpage body, state name and time."""
@@ -30,7 +30,7 @@ class AutoCMSWebpage(object):
             "</head>\n"
             "<body>\n"
             "<h2>{2} Internal Site Test: {3}</h2>\n"
-            "Page generated at: {4}\n".format( 
+            "Page generated at: {4}\n".format(
                 self.config['AUTOCMS_SITE_NAME'],
                 self.testname,
                 self.config['AUTOCMS_SITE_NAME'],
@@ -44,23 +44,32 @@ class AutoCMSWebpage(object):
 
     def write_page(self):
         """Writes the web page to file."""
-        with open(self.output_path,'w') as f:
-            f.write(self.page)
+        webpath = os.path.join(self.config['AUTOCMS_WEBDIR'], self.testname)
+        if not os.path.exists(webpath):
+            os.makedirs(webpath)
+        # write a 'index.html.new' file and then rename it to 
+        # 'index.html'. This prevents users from viewing a half 
+        # completed webpage when the page refreshes.
+        newpagepath = os.path.join(webpath, 'index.html.new')
+        pagepath = os.path.join(webpath, 'index.html')
+        with open(newpagepath, 'w') as output_file:
+            output_file.write(self.page)
+        os.rename(newpagepath, pagepath)
 
     def add_test_description(self):
         """Writes the webpage description.
 
-        Reads a 'description.html' file in the test directory, or 
+        Reads a 'description.html' file in the test directory, or
         reports in the webpage that no description was found."""
         desc_file  = os.path.join(self.config['AUTOCMS_BASEDIR'],
-                                  self.testname, 
+                                  self.testname,
                                   "description.html")
         if os.path.isfile(desc_file):
-            with open(desc_file) as df:
-                description = df.read()
+            with open(desc_file) as handle:
+                description = handle.read()
             self.page += description + '<br />\n'
         else:
-            self.page += "No test description found.<br />\n" 
+            self.page += "No test description found.<br />\n"
 
     def add_job_failure_rates(self, width, times, warn_rate):
         """Writes basic statistics on failed and successful jobs.
@@ -74,11 +83,11 @@ class AutoCMSWebpage(object):
                       'style="min-width:{0}%;">'.format(width))
         for t in times:
             min_time = int(time.time()) - t*3600
-            failures = sum(1 for job in self.records.viewvalues() 
-                               if not job.is_success() and 
+            failures = sum(1 for job in self.records
+                               if not job.is_success() and
                                    job.start_time > min_time )
-            successes = sum( 1 for job in self.records.viewvalues() 
-                               if job.is_success() and 
+            successes = sum( 1 for job in self.records
+                               if job.is_success() and
                                    job.start_time > min_time )
             self.page += ("Successful jobs in the last {0} hours: {1}"
                           "<br />\n".format(t, successes))
@@ -92,7 +101,7 @@ class AutoCMSWebpage(object):
                     self.page += '<span style="color:red;">'
                 self.page += "{0:.2f}%".format(rate)
                 if rate < warn_rate:
-                    selfpage += '</span>'
+                    self.page += '</span>'
                 self.page += '<br />\n'
             self.page += '<br />\n'
         self.page += '</div>\n'
@@ -129,3 +138,40 @@ class AutoCMSWebpage(object):
                     self.__class__.__name__,
                     self.output_path,
                     self.page))
+
+
+def produce_default_webpage(records, testname, config):
+    """Create a basic test webpage applicable to any AutoCMS test."""
+    webpage = AutoCMSWebpage(records, testname, config)
+    webpage.begin_page()
+    webpage.add_divider()
+    webpage.add_test_description()
+    webpage.add_divider()
+    webpage.add_job_failure_rates(30, [24, 3], 90.0)
+    webpage.add_divider()
+    webpage.end_page()
+    webpage.write_page()
+
+
+def purge_old_web_logs(testname, config):
+    """Remove logs older than AUTOCMS_LOG_LIFETIME in test webdir.
+
+    Any file ending in '.log' in a test directory is considered
+    a log file."""
+    webdir = os.path.join(config['AUTOCMS_WEBDIR'], testname)
+    if not os.path.exists(webdir):
+        return
+    logs = []
+    for logfile in os.listdir(webdir):
+        if re.search(r'\.log$', logfile):
+            logs.append(logfile)
+    purgetime = int(time.time()) - 3600*24*int(config['AUTOCMS_LOG_LIFETIME'])
+    for logfile in logs:
+        if int(os.path.getmtime(logfile)) < purgetime:
+            os.remove(logfile)
+
+
+def perform_test_reporting(testname, config):
+    """Analyze job records for given test and create webpage report."""
+    records = load_records(testname, config)
+    produce_default_webpage(records, testname, config)
