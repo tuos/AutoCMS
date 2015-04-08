6c013f53ad273d38e90db4984ee9c48481064549
Eric <eric.appelt@gmail.com>
2015-04-07 19:47:17 -0500

    new custom module for skim_test

--- autocms/custom/skim_test.py
+++ autocms/custom/skim_test.py
@@ -1,4 +1,4 @@
-"""Custom functions for the example_test."""
+"""Custom functions for the skim_test."""
 
 import os
 import time
@@ -8,7 +8,7 @@ from ..plot import (create_run_and_waittime_plot, create_histogram)
 
 
 def produce_webpage(records, testname, config):
-    """Create a webpage specific to the example_test."""
+    """Create a webpage specific to the skim_test."""
     webpath = os.path.join(config['AUTOCMS_WEBDIR'], testname)
     dice_plot_path = os.path.join(webpath, 'dice.png')
     recent_successes = [job for job in records
@@ -21,13 +21,13 @@ def produce_webpage(records, testname, config):
         runtime_plot_path = os.path.join(webpath, 'runtime.png')
         plot_desc = ('Successful job running and waiting times '
                      '(last 24 hours):')
-        create_run_and_waittime_plot(recent_successes, (8,4), 
+        create_run_and_waittime_plot(recent_successes, (8,4),
                                      runtime_plot_path)
         webpage.add_floating_image(45, 'runtime.png', plot_desc)
         runtime_plot_path = os.path.join(webpath, 'runtimelog.png')
         plot_desc = ('Successful job running and waiting times '
                      '(last 24 hours, log scaled):')
-        create_run_and_waittime_plot(recent_successes, (8,4), 
+        create_run_and_waittime_plot(recent_successes, (8,4),
                                      runtime_plot_path, logscale=True)
         webpage.add_floating_image(45, 'runtimelog.png', plot_desc)
     webpage.add_divider()
