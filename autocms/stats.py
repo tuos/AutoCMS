51529416f11688bb90a10ff86226356529e934e8
Eric <eric.appelt@gmail.com>
2015-04-19 14:23:31 -0500

    Life would be 1000x easier if the statistics file was a properly formatted csv with appropriate headers, so now it is.

--- autocms/stats.py
+++ autocms/stats.py
@@ -4,7 +4,13 @@ import os
 import time
 import importlib
 
+import pandas as pd
+
 from .core import load_records
+
+
+default_stat_columns = ["time", "success", "failure", "min_runtime",
+                        "mean_runtime", "max_runtime"]
 
 
 def harvest_default_stats(records, config):
@@ -24,28 +30,52 @@ def harvest_default_stats(records, config):
         mean_runtime = sum(runtimes)/float(len(runtimes))
     successes = sum(1 for job in stat_records if job.is_success())
     failures = sum(1 for job in stat_records if not job.is_success())
-    return "{} {} {} {} {} {}".format(now, successes, failures, min_runtime,
+    return "{},{},{},{},{},{}".format(now, successes, failures, min_runtime,
                                       mean_runtime, max_runtime)
 
 
-def append_stats_row(row, testname, config):
+def append_stats_row(colnames, row, testname, config):
     """Add a line to the persistent statistics log of a test."""
     statfile = os.path.join(config['AUTOCMS_BASEDIR'], testname,
-                            'statistics.dat')
+                            'statistics.csv')
+    if os.path.isfile(statfile):
+        write_headings = False
+    else:
+        write_headings = True
     with open(statfile, 'a') as stat_handle:
+        if write_headings:
+             stat_handle.write(','.join(colnames) + '\n')
         stat_handle.write(row + '\n')
+
+
+def load_stats(testname, config):
+    """Return a pandas DataFrame from a statistics file."""
+    statfile = os.path.join(config['AUTOCMS_BASEDIR'], testname,
+                            'statistics.csv')
+    if not os.path.isfile(statfile):
+        return pd.DataFrame()
+    return pd.DataFrame.from_csv(statfile)
+
+
+def load_default_stats(testname, config):
+    """Return a pandas DataFrame from a default statistics file."""
+    statcol = ["time", "success", "failure", "minrun", "meanrun", "maxrun"]
+    return load_stats(statcol, testname, config)
 
 
 def perform_stats_harvesting(testname, config):
     """Analyze job records for given test and create row of statistics."""
     records = load_records(testname, config)
-    # use a custom row if the test has configured one
+    # use custom data columns if the test has configured one
+    stat_columns = default_stat_columns
     harvest_stats = harvest_default_stats
     try:
         test_custom = importlib.import_module('autocms.custom.' + testname)
         if hasattr(test_custom, 'harvest_stats'):
-            harvest_stats = getattr(test_custom, 'produce_webpage')
+            harvest_stats = getattr(test_custom, 'harvest_stats')
+        if hasattr(test_custom, 'stat_columns'):
+            harvest_stats = getattr(test_custom, 'stat_columns')
     except ImportError:
         pass
     row = harvest_stats(records, config)
-    append_stats_row(row, testname, config)
+    append_stats_row(stat_columns, row, testname, config)
